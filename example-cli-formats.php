1ad7f58be36e475f29114552786a83d0ae28bb42
Adam Nielsen <malvineous@shikadi.net>
2015-09-29 10:22:53 +1000

    Add column type '@' to formats example

--- example-cli-formats.php
+++ example-cli-formats.php
@@ -8,8 +8,9 @@ $header = array(
   'c3'=>'#,##0.00', //custom
   'c4'=>'#,##0.00 [$€-407]', //german euro
   'c5'=>'[$￥-411]#,##0;[RED]-[$￥-411]#,##0', //japanese yen
+  'c6'=>'@', //text (numbers as text, won't strip leading zeroes)
 );
-$row = array(100,200,300,400,500);
+$row = array(100,200,300,400,500,'0123');
 $writer = new XLSXWriter();
 $writer->writeSheet(array($row),'Sheet1', $header);
 $writer->writeToFile('example.xlsx');
