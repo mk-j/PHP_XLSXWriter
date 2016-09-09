df3cbb6ae43b7bcec82224236cff1b5d6af6d9a8
Shawn McCabe <smmccabe@users.noreply.github.com>
2016-09-08 22:08:04 -0700

    Added style example

--- example-cli-formats.php
+++ example-cli-formats.php
@@ -12,6 +12,19 @@ $header = array(
 $row = array(100,200,300,400,500);
 $writer = new XLSXWriter();
 $writer->writeSheet(array($row),'Sheet1', $header);
+
+//cell styling
+//create a "fill" first: pattern, forground color, background color
+$fill = $this->addFill('solid', 'FF9900', 'FFFFFF');
+//create a style based on our fill
+$style = $this->addCellStyle(array('fillId' => $fill));
+
+//cell in column B will have the custom style
+$row_style = array(0, $style, 0, 0, 0);
+$this->writeSheetRow('sheet1', $row, $row_style);
+
 $writer->writeToFile('example.xlsx');
+
+
 
 
