df2ff7e598ddb202cb84865d4e7264c3db4523ff
kexej <31655949+kexej@users.noreply.github.com>
2017-09-11 17:49:34 +0300

    Limit max cell len

--- xlsxwriter.class.php
+++ xlsxwriter.class.php
@@ -682,6 +682,9 @@ class XLSXWriter
 	//------------------------------------------------------------------
 	public static function xmlspecialchars($val)
 	{
+		if (strlen($val) > 32767)
+            		$val = mb_substr($val, 0, 32767);
+		
 		//note, badchars does not include \t\n\r (\x09\x0a\x0d)
 		static $badchars = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0b\x0c\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f";
 		static $goodchars = "                              ";
