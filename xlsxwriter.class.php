2a9dc3235804afda16cb60497ed3388c4a6083f4
Athena Metis <athena-metis@pobox.com>
2018-10-03 10:46:15 +0100

    Allow for the number format to be overridden for a writeSheetRow operation (this is currently coarse in that it only allows for one override type for the whole row, not per column) This is intended to allow for interim headers to be added similar to how headers are added

--- xlsxwriter.class.php
+++ xlsxwriter.class.php
@@ -254,9 +254,13 @@ class XLSXWriter
 			$default_column_types = $this->initializeColumnTypes( array_fill($from=0, $until=count($row), 'GENERAL') );//will map to n_auto
 			$sheet->columns = array_merge((array)$sheet->columns, $default_column_types);
 		}
-		
+
+		$overrideNumberFormats = null;
 		if (!empty($row_options))
 		{
+            $overrideNumberFormats = isset($row_options['number_format']) ? $row_options['number_format'] : null;
+            if (!empty($row_options['number_format'])) { unset($row_options['number_format']); }
+
 			$ht = isset($row_options['height']) ? floatval($row_options['height']) : 12.1;
 			$customHt = isset($row_options['height']) ? true : false;
 			$hidden = isset($row_options['hidden']) ? (bool)($row_options['hidden']) : false;
@@ -274,7 +278,7 @@ class XLSXWriter
 			$number_format = $sheet->columns[$c]['number_format'];
 			$number_format_type = $sheet->columns[$c]['number_format_type'];
 			$cell_style_idx = empty($style) ? $sheet->columns[$c]['default_cell_style'] : $this->addCellStyle( $number_format, json_encode(isset($style[0]) ? $style[$c] : $style) );
-			$this->writeCell($sheet->file_writer, $sheet->row_count, $c, $v, $number_format_type, $cell_style_idx);
+			$this->writeCell($sheet->file_writer, $sheet->row_count, $c, $v, !empty($overrideNumberFormats) ? $overrideNumberFormats : $number_format_type, $cell_style_idx);
 			$c++;
 		}
 		$sheet->file_writer->write('</row>');
