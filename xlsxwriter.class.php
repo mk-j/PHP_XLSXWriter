f9524a609f5ae4b8292ce3ceea1538e4a47bbaea
Anton Smirnov <sandfox@sandfox.me>
2022-06-10 17:43:49 +0300

    Do not pass null to htmlspecialchars

--- xlsxwriter.class.php
+++ xlsxwriter.class.php
@@ -764,7 +764,7 @@ class XLSXWriter
 		//note, badchars does not include \t\n\r (\x09\x0a\x0d)
 		static $badchars = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0b\x0c\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x7f";
 		static $goodchars = "                              ";
-		return strtr(htmlspecialchars($val, ENT_QUOTES | ENT_XML1), $badchars, $goodchars);//strtr appears to be faster than str_replace
+		return !empty($val) ? strtr(htmlspecialchars($val, ENT_QUOTES | ENT_XML1), $badchars, $goodchars) : '';//strtr appears to be faster than str_replace
 	}
 	//------------------------------------------------------------------
 	public static function array_first_key(array $arr)
